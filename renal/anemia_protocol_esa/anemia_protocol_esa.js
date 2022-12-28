8476704e9ad8fb2ef785bdb71f759cd4f9031830
jmasalma <49733702+jmasalma@users.noreply.github.com>
2022-12-27 19:52:06 -0500

    Add files via upload

--- renal/anemia_protocol_esa/anemia_protocol_esa.js
+++ renal/anemia_protocol_esa/anemia_protocol_esa.js
@@ -1,17 +1,67 @@
-function check_vals() {
-  document.getElementById("result").innerHTML = ""
+function isEmpty(value) {
+    return value === null || typeof(value) === 'undefined' || value === ''
+}
+
+
+
+function calculateDose(value, increase) {
+  let doseMatrix = [
+     [10, 10, 0]
+    ,[10, 20, 10]
+    ,[40, 50, 30]
+  ];
   
+  return 666
+}
+
+
+
+function check_vals() {
+
+  // Statics...
+  doseTable = `
+<table>
+<tr><th>Current Dose</th><th>If dose increase is required, change dose to</th><th>If dose decrease is required, change dose to</th></tr>
+<tr><td>10 mcg once every 4 weeks</td><td>10 mcg once every 2 weeks</td><td>Discontinue darbepoetin</td></tr>
+<tr><td>10 mcg once every 2 weeks</td><td>10 mcg once weekly</td><td>10 mcg once every 4 weeks</td></tr>
+<tr><td>10 mcg once weekly</td><td>20 mcg once weekly</td><td>10 mcg once every 2 weeks</td></tr>
+<tr><td>20 mcg once weekly</td><td>30 mcg once weekly</td><td>10 mcg once weekly</td></tr>
+<tr><td>30 mcg once weekly</td><td>40 mcg once weekly</td><td>20 mcg once weekly</td></tr>
+<tr><td>40 mcg once weekly</td><td>50 mcg once weekly</td><td>30 mcg once weekly</td></tr>
+<tr><td>50 mcg once weekly</td><td>60 mcg once weekly</td><td>40 mcg once weekly</td></tr>
+<tr><td>60 mcg once weekly</td><td>80 mcg once weekly</td><td>50 mcg once weekly</td></tr>
+<tr><td>80 mcg once weekly</td><td>100 mcg once weekly</td><td>60 mcg once weekly</td></tr>
+<tr><td>100 mcg once weekly</td><td>120 mcg once weekly</td><td>80 mcg once weekly</td></tr>
+<tr><td>120 mcg once weekly</td><td>150 mcg once weekly</td><td>100 mcg once weekly</td></tr>
+<tr><td>150 mcg once weekly</td><td>Notify nephrologist to assess hyporesponsiveness</td><td>120 mcg once weekly</td></tr>
+</table>
+`
+
+
+  document.getElementById("result").innerHTML = ""
+
+  ESA = document.getElementById("ESA").checked;
+  ESAonHold = document.getElementById("ESAonHold").checked;
+  //document.getElementById("footer").innerHTML = "debug: " + cHgb + " : " + lmHgb + " : " + ESA + " : " + ESAonHold + " : " + changedESA;
+
 /*
-  ESARange = document.getElementById("ESARange").value;
-  if (ESARange == 1) {
-    changedESA = "reduced"
-    document.getElementById("changedESARange").innerHTML = "Reduced"
-  } else if (ESARange == 2) {
-    changedESA = "nochange"
-    document.getElementById("changedESARange").innerHTML = "No change"
-  } else if (ESARange == 3) {
-    changedESA = "increased"
-    document.getElementById("changedESARange").innerHTML = "Increased"
+  current_dose = document.getElementById("current_dose").value;
+  previous_dose = document.getElementById("previous_dose").value;
+
+
+  if (!ESA) {
+    document.getElementById("current_dose_div").style.display = 'none';
+  } else {
+    document.getElementById("current_dose_div").style.display = 'block';
+    //document.getElementById("ESAonHold").checked = false;
+  }
+
+  if (!ESAonHold) {
+    document.getElementById("previous_dose_div").style.display = 'none';
+  } else {
+    document.getElementById("previous_dose_div").style.display = 'block';
+    document.getElementById("current_dose_div").style.display = 'none';
+    document.getElementById("ESA").checked = false;
   }
 */
 
@@ -26,15 +76,19 @@ function check_vals() {
 
 
   
-  ESAonHold = document.getElementById("ESAonHold").checked;
   cHgb = document.getElementById("cHgb").value;
   lmHgb = document.getElementById("lmHgb").value;
   if (cHgb === "" || lmHgb === "") {
     return
   }
-  ESA = document.getElementById("ESA").checked;
-  //document.getElementById("footer").innerHTML = "debug: " + cHgb + " : " + lmHgb + " : " + ESA + " : " + ESAonHold + " : " + changedESA;
 
+/*
+  if (!isEmpty(current_dose)) {
+    doseTable = "new dose is XYZc"
+  } else if (!isEmpty(previous_dose)) {
+    doseTable = "new dose is XYZp"
+  }
+*/
 
 
   if ((Math.abs(lmHgb - cHgb) >= 15) || (cHgb < 85) || ((cHgb > 139) && (ESA || ESAonHold))) {
@@ -47,7 +101,12 @@ function check_vals() {
       if (changedESA == "increased") {
         document.getElementById("result").innerHTML = "- Check that the iron studies have been appropriately monitored and addressed prior to increasing the dose<br>- If TSAT less than 20% consider iron load instead of increasing ESA dosage<br>- If dose was increased in past 5 weeks, maintain dose until next blood work cycle<br>- Notify MD if Hb is not in target range after 3 consecutive dosage increases"
       } else {
-        document.getElementById("result").innerHTML = "- Check that the iron studies have been appropriately monitored and addressed prior to increasing the dose<br>- <b>If TSAT less than 20% consider iron load instead of increasing ESA dosage</b><br>- If no dosage adjustment in the past 5 weeks <b>increase Darbepoetin dose</b> as per dosage adjustment tables"
+/*
+        if (!isEmpty(current_dose)) {
+          doseTable = calculateDose(current_dose)
+        }
+*/
+        document.getElementById("result").innerHTML = "- Check that the iron studies have been appropriately monitored and addressed prior to increasing the dose<br>- <b>If TSAT less than 20% consider iron load instead of increasing ESA dosage</b><br>- If no dosage adjustment in the past 5 weeks <b>increase Darbepoetin dose</b> as per dosage adjustment tables" + "<br/>" + doseTable
       }
     } else {
       document.getElementById("result").innerHTML = "<b>Re-measure iron bloodwork if not done in past week</b>. Refer to Appendix F<br><b>Consult MD to initiate ESA</b>. Suggested initial dose Darbepoetin 0.45 mcg/kg/wk"
@@ -61,7 +120,7 @@ function check_vals() {
     } else if (ESA && !ESAonHold) {
       document.getElementById("result").innerHTML = "<b>Maintain ESA (Aranesp/Darbepoetin) Dose</b>. Continue to monitor Hb at regular blood work cycle"
     } else if (ESAonHold) {
-      document.getElementById("result").innerHTML = "<b>Restart ESA (Aranesp/Darbepoetin) at a reduced dose</b> based on the dose before hold. Refer to ESA dosage adjustment table. Appendix E. Continue monitoring at regular blood work cycle"
+      document.getElementById("result").innerHTML = "<b>Restart ESA (Aranesp/Darbepoetin) at a reduced dose</b> based on the dose before hold. Refer to ESA dosage adjustment table. Appendix E. Continue monitoring at regular blood work cycle" + "<br/>" + doseTable
     }
   }
 
@@ -73,7 +132,7 @@ function check_vals() {
     if (ESA && !ESAonHold) {
       if (cHgb <= 125) {
         if (changedESA != "reduced") {
-          document.getElementById("result").innerHTML = "- If there has been no dosage reduction in the past 5 weeks, <b>reduce ESA dosage</b> as per ESA dosage adjustment table in Appendix E."
+          document.getElementById("result").innerHTML = "- If there has been no dosage reduction in the past 5 weeks, <b>reduce ESA dosage</b> as per ESA dosage adjustment table in Appendix E." + "<br/>" + doseTable
         } else {
           document.getElementById("result").innerHTML = "- If there have been dosage reductions in the past 5 weeks <b>maintain current dose</b>. recheck Hb at the next regular blood work cycle"
         }
@@ -86,7 +145,7 @@ function check_vals() {
       } else if (cHgb >= 116 && cHgb <= 125) {
         document.getElementById("result").innerHTML = "testing 1"
         if ((lmHgb - cHgb) >= 10) {
-          document.getElementById("result").innerHTML = "If change is greater than 10 g/L from last Hb then <b>resume ESA at reduced dose</b>. Refer to protocol ESA adjustment tables. Continue to monitor Hb at regular blood work cycle"
+          document.getElementById("result").innerHTML = "If change is greater than 10 g/L from last Hb then <b>resume ESA at reduced dose</b>. Refer to protocol ESA adjustment tables. Continue to monitor Hb at regular blood work cycle" + "<br/>" + doseTable
         } else if ((lmHgb - cHgb) < 10) {
           document.getElementById("result").innerHTML = "If change is less than 10 g/L from last Hb then continue to <b>hold ESA</b>"
         } else {
